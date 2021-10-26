adea627a1788d6cbc457a3f6a4936cd28b8ac261
Betty <61933658+Betty985@users.noreply.github.com>
2021-10-26 14:44:48 +0800

    没有什么问题。。。HTML要引入js文件！

--- 选项卡/选项卡.js
+++ 选项卡/选项卡.js
@@ -1,16 +1,15 @@
-window.onload = function () {
+window.onload = function a() {
   var li = document.querySelectorAll("li");
   var div = document.querySelectorAll(".i");
   for (let i = 0; i < li.length; i++) {
-    li[i].addEventListener("mouseover", () => {
-      li[i].style.background = "#FFFFCC";
-    });
-    li[i].addEventListener("click", function(){
-      li[i].class = "active";
-      div[i].class = "active";
-      for (let j = 0; j < li.length && j != i; j++) {
-        li[j].class = "";
-        div[j].class = "";
+    li[i].addEventListener("mouseover", function () {
+      li[i].className = "active";
+      div[i].className = "i active";
+      for (let j = 0; j < li.length; j++) {
+        if (j != i) {
+          li[j].className = "";
+          div[j].className = "i";
+        }
       }
     });
   }
